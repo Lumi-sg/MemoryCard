81438497f48dfaed2ee88c27494ef9d8b9ac49db
Lumi <shaicloud@gmail.com>
2023-06-07 00:20:49 -0400

    added scores, best score and local storage

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,12 @@ import "./styles/App.css";
 import "./styles/Reset.css";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import Information from "./components/Information";
+
 import Game from "./components/Game";
 function App() {
     return (
         <>
             <Header />
-            <Information />
             <Game />
             <Footer />
         </>
--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -2,21 +2,41 @@ import { useEffect, useState } from "react";
 import "../styles/Game.css";
 
 const Game = () => {
-    const images = import.meta.glob("../assets/*png");
     const [loadedImages, setLoadedImages] = useState<string[]>([]);
     const [clickTrigger, setClickTrigger] = useState(false);
     const [filenameArray, setFilenameArray] = useState<string[]>([]);
+    const [currentScore, setcurrentScore] = useState(0);
+    const [bestScore, setBestScore] = useState(0);
 
-    const handleClick = (filename: string) => {
+    const images = import.meta.glob("../assets/*png");
+
+    const gameFlow = (filename: string) => {
         const cleanedfileName = filename.split("/").pop();
         if (cleanedfileName === undefined) {
             return;
         }
+        if (filenameArray.includes(cleanedfileName)) {
+            handleBestScore();
+            alert("Already clicked");
+            setcurrentScore(0);
+            setFilenameArray([]);
+            return;
+        }
+
+        setcurrentScore(currentScore + 1);
         setFilenameArray((prevArray) => [...prevArray, cleanedfileName]);
         setClickTrigger((prevTrigger) => !prevTrigger);
     };
 
+    const handleBestScore = () => {
+        if (currentScore > bestScore) {
+            setBestScore(currentScore);
+            localStorage.setItem("bestScore", currentScore.toString());
+        }
+    };
+
     useEffect(() => {
+        //load the images to the page
         const loadImages = async () => {
             const imagePaths = Object.keys(images);
 
@@ -33,23 +53,30 @@ const Game = () => {
     }, [clickTrigger]);
 
     useEffect(() => {
-        console.table(filenameArray);
-    }, [filenameArray]);
+        // load the highest score from local storage
+        setBestScore(parseInt(localStorage.getItem("bestScore") || "0"));
+    });
 
     return (
-        <div className="Game">
-            <div className="cardContainer">
-                {loadedImages.map((image, index) => (
-                    <img
-                        className="card"
-                        key={index}
-                        src={image}
-                        alt={`Image ${index}`}
-                        onClick={() => handleClick(image)}
-                    />
-                ))}
+        <>
+            <div className="Information">
+                <p>Current score: {currentScore}</p>
+                <p>Best score: {bestScore}</p>
             </div>
-        </div>
+            <div className="Game">
+                <div className="cardContainer">
+                    {loadedImages.map((image, index) => (
+                        <img
+                            className="card"
+                            key={index}
+                            src={image}
+                            alt={`Image ${index}`}
+                            onClick={() => gameFlow(image)}
+                        />
+                    ))}
+                </div>
+            </div>
+        </>
     );
 };
 
