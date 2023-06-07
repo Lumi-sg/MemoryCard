d5084de5b5aeaebd76aeb1b0787bf96cb180208a
Lumi <shaicloud@gmail.com>
2023-06-06 21:04:34 -0400

    better image files

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -1,8 +1,35 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "../styles/Game.css";
 
 const Game = () => {
-	return <div className="Game"></div>;
+	const images = import.meta.glob("../assets/*png");
+	const [loadedImages, setLoadedImages] = useState<string[]>([]);
+
+	useEffect(() => {
+		const loadImages = async () => {
+			const imagePaths = Object.keys(images);
+
+			const loadedImagePromises = imagePaths.map(async (path) => {
+				const imageModule: any = await images[path]();
+				return imageModule.default;
+			});
+
+			const loadedImages = await Promise.all(loadedImagePromises);
+			setLoadedImages(loadedImages);
+		};
+
+		loadImages();
+	}, []);
+
+	return (
+		<div className="Game">
+			<div className="cardContainer">
+				{loadedImages.map((image, index) => (
+					<img className="card" key={index} src={image} alt={`Image ${index}`} />
+				))}
+			</div>
+		</div>
+	);
 };
 
 export default Game;
